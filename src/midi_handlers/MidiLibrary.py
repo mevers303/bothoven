0db2b71ae8d3e4216b64eb1aa7d4759130c278f8
Mark Evers <mevers303@gmail.com>
2018-10-07 15:08:02 -0600

    Test-train split ready for debugging

--- src/midi_handlers/MidiLibrary.py
+++ src/midi_handlers/MidiLibrary.py
@@ -5,6 +5,8 @@
 
 import numpy as np
 import mido
+import random
+from sklearn.model_selection import train_test_split
 
 from files.file_functions import get_filenames
 import wwts_globals
@@ -13,37 +15,39 @@ import wwts_globals
 
 class MidiLibrary:
 
-    def __init__(self, base_dir):
+    def __init__(self, base_dir, autoload=True):
 
         self.base_dir = base_dir
         self.filenames = None
-        self.filenames_count = 0
         self.mids = None
 
         self.get_filenames()
+        if autoload:
+            self.load()
 
 
     def get_filenames(self):
 
-        self.filenames = get_filenames(self.base_dir)
-        self.filenames_count = len(self.filenames)
-        print("Found", self.filenames_count, "in", self.base_dir)
+        self.filenames = np.array(get_filenames(self.base_dir))
+        print("Found", self.filenames.size, "in", self.base_dir)
 
 
     def load(self):
 
-        self.mids = []
+        midi_files = []
         done = 0
 
         for filename in self.filenames:
             try:
-                self.mids.append(mido.MidiFile(filename))
+                midi_files.append(mido.MidiFile(filename))
             except Exception as e:
                 print("\nThere was an error reading", filename)
                 print(e)
             finally:
                 done += 1
-                wwts_globals.progress_bar(done, self.filenames_count)
+                wwts_globals.progress_bar(done, self.filenames.size)
+
+        self.mids = np.array(midi_files)
 
 
     def step_through(self):
@@ -98,25 +102,55 @@ class MidiLibrary:
 
 
 
+class MidiLibrarySplit(MidiLibrary):
+
+    def __init__(self, base_dir):
+
+        super().__init__(base_dir)
+
+        self.filenames_train = None
+        self.filenames_test = None
+        self.mids_train = None
+        self.mids_test = None
+
+
+    def split_files(self):
+
+        test_size = int(self.filenames.size / 4)
+        test_indices = np.random.choice(self.filenames.size, size=test_size, replace=False)
+        train_indices = np.delete(np.arange(self.filenames.size), test_indices)
+
+        self.filenames_train = self.filenames[train_indices]
+        self.filenames_test = self.filenames[test_indices]
+        self.mids_train = self.mids[train_indices]
+        self.mids_test = self.mids[test_indices]
+
+
+    def load(self):
+
+        super().load()
+        self.split_files()
+
+
 
 def main():
 
     import pickle
 
-    # lib = MidiLibrary("midi/bach_cleaned")
-    # lib.load()
-    #
-    # with open("midi/pickles/bach.pkl", "wb") as f:
-    #     pickle.dump(lib, f)
+    lib = MidiLibrary("midi/bach_cleaned")
+    lib.load()
 
-    with open("midi/pickles/bach.pkl", "rb") as f:
-        lib = pickle.load(f)
+    with open("midi/pickles/bach.pkl", "wb") as f:
+        pickle.dump(lib, f)
 
-    x = 0
+    # with open("midi/pickles/bach.pkl", "rb") as f:
+    #     lib = pickle.load(f)
 
-    for buf, target in lib.step_through():
-        pass
-        # print("time:", str(time).rjust(5), "note: ", note)
+        # x = 0
+        #
+        # for buf, target in lib.step_through():
+        #     pass
+        #     # print("time:", str(time).rjust(5), "note: ", note)
 
 if __name__ == "__main__":
     main()
