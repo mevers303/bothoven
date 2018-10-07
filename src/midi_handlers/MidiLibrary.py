70c48b0c1a69ffd03c45c8dd5d9c97596ec89729
Mark Evers <mevers303@gmail.com>
2018-10-07 03:29:16 -0600

    Fixed a bug. it seems to be working.

--- src/midi_handlers/MidiLibrary.py
+++ src/midi_handlers/MidiLibrary.py
@@ -53,7 +53,7 @@ class MidiLibrary:
             for track in mid.tracks:
 
                 # time series matrix
-                buf = np.zeros((wwts_globals.NUM_STEPS, wwts_globals.NUM_FEATURES))
+                buf = np.zeros((wwts_globals.NUM_STEPS, wwts_globals.NUM_FEATURES), dtype=np.uint32)
                 # set special one-hot for track_start
                 buf[-1, -2] = 1
                 # cumulative delta time from skipped notes
@@ -61,38 +61,62 @@ class MidiLibrary:
 
                 for msg in track:
 
-                    if msg.type != "note_on" or msg.type != "note_off":
+                    if not (msg.type == "note_on" or msg.type == "note_off"):
                         # store the delta time of any skipped messages
                         cum_time += msg.time
                         continue
 
-                    # move the time series up one
-                    buf = np.roll(buf, -1, axis=0)
+                    # the next note beyond the buffer
+                    target = np.zeros((wwts_globals.NUM_FEATURES), dtype=np.uint32)
                     # set the time
-                    buf[-1, 0] = msg.time + cum_time
+                    target[0] = msg.time + cum_time
                     # reset skipped time delta
                     cum_time = 0
                     # find the one-hot note code
                     note_code = msg.note + 1 if msg.type == "note_on" else msg.note + 128 + 1
-                    buf[note_code] = 1
-
+                    target[note_code] = 1
                     # spit out this buffer
-                    yield buf
+                    yield buf, target
 
+                    # move the time series up one
+                    buf = np.roll(buf, -1, axis=0)
+                    # store this message as the last in the time series
+                    buf[-1] = target
+
+                # now this is the end of the track
                 # if there were actual usable messages in the track, end the track
                 if buf[-1, -2] != 1:
-                    buf = np.roll(buf, -1, axis=0)
                     # special one-hot for track end
-                    buf[-1, -1] = 1
-                    yield buf
+                    target = np.zeros((wwts_globals.NUM_FEATURES), dtype=np.uint32)
+                    target[-1] = 1
+                    # yield the end of track
+                    yield buf, target
+                else:
+                    pass
+
+            print(len(mid.tracks), mid.filename)
 
 
 
 
 def main():
 
-    lib = FlatMidiLibrary("midi/bach_cleaned")
-    lib.load()
+    import pickle
+
+    # lib = MidiLibrary("midi/bach_cleaned")
+    # lib.load()
+    #
+    # with open("midi/pickles/bach.pkl", "wb") as f:
+    #     pickle.dump(lib, f)
+
+    with open("midi/pickles/bach.pkl", "rb") as f:
+        lib = pickle.load(f)
+
+    x = 0
+
+    for buf, target in lib.step_through():
+        pass
+        # print("time:", str(time).rjust(5), "note: ", note)
 
 if __name__ == "__main__":
     main()
