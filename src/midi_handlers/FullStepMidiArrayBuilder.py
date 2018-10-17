46b508e61fe6286bf13723d8a68b8ae664fde432
Mark Evers <mevers303@gmail.com>
2018-10-16 21:24:25 -0600

    Created files

---
+++ src/midi_handlers/FullStepMidiArrayBuilder.py
@@ -0,0 +1,71 @@
+from collections import defaultdict
+import numpy as np
+import mido
+import scipy.sparse
+
+from bothoven_globals import NUM_FEATURES, NUM_STEPS
+
+
+class FullStepMidiArrayBuilder:
+
+    def __init__(self, filename):
+
+        self.filename = filename
+        self.tracks = []
+
+
+    def mid_to_array(self):
+
+        mid = mido.MidiFile(self.filename)
+
+        for track in mid.tracks:
+
+            # running total of the amount of time that has passed
+            abs_time = 0
+            track_dict = defaultdict(list)
+
+            for msg in track:
+
+                abs_time += msg.time
+
+                if not (msg.type == 'note_on' or msg.type == 'note_off') or msg.channel == 9:  # skip drum tracks
+                    continue
+
+                # find the one-hot note
+                note_code = msg.note
+                if msg.type == "note_off" or not msg.velocity:
+                    note_code += 128
+
+                # add this note to our dictionary of absolute times
+                track_dict[abs_time].append(note_code)
+
+            if len(track_dict):
+                self.tracks.append(track_dict)
+
+        return self.flush_out_buf()
+
+
+    def flush_out_buf(self):
+
+        if not len(self.tracks):
+            return []
+
+        # empty buffer of NUM_STEPS for the beginning of the track
+        buf = [scipy.sparse.csr_matrix((NUM_STEPS - 1, NUM_FEATURES), dtype=np.byte)]
+
+        for track in self.tracks:
+
+            # index pair locations for sparse matrix
+            x = [0, max(track) + 2]
+            y = [NUM_FEATURES - 2, NUM_FEATURES - 1]
+            data = [1, 1]
+            for abs_time, note_codes in track.items():
+                x.extend([abs_time + 1] * len(note_codes))  # abs_time + 1 to skip over track_start
+                y.extend(note_codes)
+                data.extend([1] * len(note_codes))
+
+            track_buf = scipy.sparse.csr_matrix((data, (x, y)), shape=(max(track) + 1 + 2, NUM_FEATURES), dtype=np.byte)  # +1 because the abs_time is an index and +2 for track_start and track_end
+
+            buf.append(track_buf)
+
+        return scipy.sparse.vstack(buf, format="csr", dtype=np.byte)
---
+++ src/midi_handlers/FullStepMidiLibrary.py
@@ -0,0 +1,58 @@
+# Mark Evers
+# 9/21/18
+# MidiLibrary.py
+# Objects for managing a set of MIDI functions.
+
+import numpy as np
+from midi_handlers.FullStepMidiArrayBuilder import FullStepMidiArrayBuilder
+import scipy.sparse
+
+from midi_handlers.MusicLibrary import MusicLibraryFlat
+
+
+class FullStepMidiArrayFlat(MusicLibraryFlat):
+
+    def __init__(self, base_dir="", filenames=None, autoload=True):
+        super().__init__(FullStepMidiArrayBuilder, base_dir, filenames, autoload)
+
+
+    def load_files(self):
+
+        super().load_files()
+
+        self.buf = scipy.sparse.vstack(self.buf, format="csr", dtype=np.byte)
+        self.NUM_FEATURES = 128 + 128 + 2  # note_on + note_off + 2 track start/end
+
+
+    def step_through(self):
+
+        i = 0
+
+        while i < self.buf.shape[0] - self.NUM_STEPS - 1:  # gotta leave room for the target at the end
+
+            x = self.buf[i:i + self.NUM_STEPS].toarray()
+            y = self.buf[i + self.NUM_STEPS].toarray()[0]
+
+            i += 1
+
+            yield x, y
+
+
+def main():
+
+    import os
+    import pickle
+
+    lib_name = "bach"
+
+    lib = FullStepMidiArrayFlat(os.path.join("midi", lib_name))
+    # lib.load()  # autoload is on by default
+
+    print("Pickling...")
+    os.makedirs("midi/pickles")
+    with open(os.path.join(f"midi/pickles/{lib_name}_midi.pkl"), "wb") as f:
+        pickle.dump(lib, f)
+    print("Done!")
+
+if __name__ == "__main__":
+    main()
