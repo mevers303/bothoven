9c6ec00d63610bc0fb52d9a6d5351330868f95de
Mark Evers <mevers303@gmail.com>
2018-11-05 01:08:16 -0700

    Improved install script

--- aws_firstrun.sh
+++ aws_firstrun.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt upgrade -y
 sudo apt dist-upgrade -y
-sudo apt install htop screen
+sudo apt install htop screen -y
 
 source activate tensorflow_p36
 conda upgrade --all -y
-pip install --upgrade tensorflow-gpu
 pip install tensorboard keras music21
 source deactivate
 
--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 source activate tensorflow_p36
-cd bothoven
 export PYTHONPATH=src
 python src/model.py chopin_2hand_m21 --epochs 15
-# sudo shutdown -hP now
+sudo shutdown -hP now
