db171c4e96b1bb974748071bad245080159cebbe
Mark Evers <mevers303@gmail.com>
2018-11-04 19:22:04 -0700

    Added upload_latest_file()

--- src/functions/s3.py
+++ src/functions/s3.py
@@ -1,19 +1,20 @@
 import boto, boto.s3 as s3
 import json
+import os
 
 from functions.file_functions import get_filenames
-
 
 with open("s3_key.json", "r") as f:
     _keyfile = json.load(f)
 _bucket_name = "bothoven"
 
 
+
 def sync_s3(dir, overwrite=False):
 
-    files = get_filenames(dir)
     conn = boto.connect_s3(_keyfile["key"], _keyfile["secret_key"])
     bucket = conn.get_bucket(_bucket_name)
+    files = get_filenames(dir)
 
     if overwrite:
         upload_files_to_s3(bucket, files)
@@ -29,6 +30,19 @@ def upload_files_to_s3(bucket, files):
         print(f"Uploading '{file}' to S3 bucket '{bucket.name}'...")
         key = s3.key.Key(bucket, file)
         key.set_contents_from_filename(file)
+
+
+def upload_latest_file(dir):
+
+    conn = boto.connect_s3(_keyfile["key"], _keyfile["secret_key"])
+    bucket = conn.get_bucket(_bucket_name)
+
+    files = get_filenames(dir)
+    latest_file = max(files, key=os.path.getctime)
+    key = s3.key.Key(bucket, name=latest_file)
+    print(f"Uploading '{latest_file}' to S3 bucket '{bucket.name}'...")
+    key.set_contents_from_filename(latest_file)
+
 
 
 
