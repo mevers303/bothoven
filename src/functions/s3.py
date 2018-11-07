34b4910aabd8f325d6f13fb49ae6bcacb3c7692c
Mark Evers <mevers303@gmail.com>
2018-11-06 18:33:29 -0700

    down syncs in main() now

--- src/functions/s3.py
+++ src/functions/s3.py
@@ -15,63 +15,50 @@ def get_bucket():
     return conn.get_bucket(_bucket_name)
 
 
-def upload_file(file, bucket=None):
+def upload_file(file, bucket=None, verbose=True):
 
-    print(f" -> Uploading '{file}' to S3...")
+    if verbose:
+        print(f" -> Uploading '{file}' to S3...")
     if not bucket:
         bucket = get_bucket()
     key = s3.key.Key(bucket, file)
     key.set_contents_from_filename(file)
 
 
-def up_sync_s3(dir, overwrite=False):
+def upload_files(bucket, files, verbose=True):
+
+    for file in files:
+        upload_file(file, bucket, verbose=verbose)
+
+
+def up_sync_s3(dir, overwrite=False, verbose=True):
 
     bucket = get_bucket()
     files = get_filenames(dir)
 
     if overwrite:
-        upload_files(bucket, files)
+        upload_files(bucket, files, verbose=verbose)
         return len(files)
     else:
         keys = [key.name for key in bucket.get_all_keys()]
         new_files = set(files) - set(keys)
-        upload_files(bucket, new_files)
+        upload_files(bucket, new_files, verbose=verbose)
         return len(new_files)
 
 
-def upload_files(bucket, files):
-
-    for file in files:
-        upload_file(file, bucket)
-
-
-def upload_latest_file(dir):
+def upload_latest_file(dir, verbose=True):
 
     bucket = get_bucket()
 
     files = get_filenames(dir)
     latest_file = max(files, key=os.path.getctime)
-    upload_file(latest_file, bucket)
+    upload_file(latest_file, bucket, verbose=verbose)
 
 
-def down_sync_s3(dir, overwrite=False):
-    
-    bucket = get_bucket()
-    files = get_filenames(dir)
+def download_file(file, bucket=None, verbose=True):
 
-    if overwrite:
-        download_files(bucket, files)
-        return len(files)
-    else:
-        keys = [key.name for key in bucket.get_all_keys() if key.name.startswith(dir)]
-        new_files = set(keys) - set(files)
-        download_files(bucket, new_files)
-        return len(new_files)
-
-
-def download_file(file, bucket=None):
-
-    print(f" -> Downloading '{file}' from S3...")
+    if verbose:
+        print(f" -> Downloading '{file}' from S3...")
     if not bucket:
         bucket = get_bucket()
 
@@ -82,12 +69,27 @@ def download_file(file, bucket=None):
     key.get_contents_to_filename(file)
 
 
-def download_files(bucket, files):
-    
+def download_files(bucket, files, verbose=True):
+
     for file in files:
-        download_file(file, bucket)
+        download_file(file, bucket, verbose=verbose)
+
+
+def down_sync_s3(dir, overwrite=False, verbose=True):
+    
+    bucket = get_bucket()
+    files = get_filenames(dir)
+
+    if overwrite:
+        download_files(bucket, files, verbose=verbose)
+        return len(files)
+    else:
+        keys = [key.name for key in bucket.get_all_keys() if key.name.startswith(dir)]
+        new_files = set(keys) - set(files)
+        download_files(bucket, new_files, verbose=verbose)
+        return len(new_files)
 
 
 if __name__ == "__main__":
-    up_sync_s3("models")
-    up_sync_s3("tensorboard", overwrite=True)
+    down_sync_s3("models")
+    down_sync_s3("tensorboard")
