9e8c878d50245cb1c583c7437bc3dc1601f8e3a3
Mark Evers <mevers303@gmail.com>
2018-11-25 00:18:03 -0700

    You can create a library using the dataframe for parameters

---
+++ src/eda/create_library.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import scipy.sparse as sps
+from midi_handlers.Music21ArrayBuilder import Music21ArrayBuilder
+from functions.pickle_workaround import pickle_dump
+
+from midi_handlers.MusicLibrary import MusicLibrary, MusicLibraryFlat, MusicLibrarySplit
+from midi_handlers.Music21Library import Music21LibrarySplit
+import functions.s3 as s3
+
+
+def main():
+
+    lib_name = "Cmaj"
+    df = pd.read_csv("/media/mark/Data/midi/midiclassics_m21/key_signatures.csv", index_col="path")
+    Cmaj = [x[:-4] for x in df.index.values[(df["tonic"] == "C") & (df["mode"] == "major")]]
+    lib = Music21LibrarySplit(filenames=Cmaj)
+
+    print("Pickling...")
+    path = f"midi/pickles/{lib_name}.pkl"
+    pickle_dump(lib, path)
+    s3.upload_file(path)
+    print("Done!")
+
+
+if __name__ == "__main__":
+    main()
